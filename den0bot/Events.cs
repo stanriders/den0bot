513c74615b739c145eae44c376ef96f1bfc0e42a
StanR <hi@stanr.info>
2018-10-08 16:38:57 +0300

    Add localization to events

--- den0bot/Events.cs
+++ den0bot/Events.cs
@@ -5,51 +5,51 @@ namespace den0bot
 {
     public static class Events
     {
-        public static string Event()
+        public static string Event(long chatID)
         {
             switch (RNG.NextNoMemory(0, 1000))
             {
-                case 9: return "Егорыч, бросай наркотики";
-                case 99: return "Панда - щитпостер";
-                case 999: return "Не хочу";
-                case 8: return "Жаба, поебемся после работы?";
-                case 88: return "Панда - объебос";
-                case 888: return "Ноборееее";
-                case 7: return "Шлад, когда 900пп?";
-                case 77: return "ккк - самая лучшая девочка!";
-                case 777: return "Не получилось чет";
-                default: return string.Empty;
+                case 9:		return Localization.Get("event_1", chatID);
+                case 99:	return Localization.Get("event_2", chatID);
+                case 999:	return Localization.Get("event_3", chatID);
+                case 8:		return Localization.Get("event_4", chatID);
+                case 88:	return Localization.Get("event_5", chatID);
+                case 888:	return Localization.Get("event_6", chatID);
+                case 7:		return Localization.Get("event_7", chatID);
+                case 77:	return Localization.Get("event_8", chatID);
+                case 777:	return Localization.Get("event_9", chatID);
+                default:	return string.Empty;
             }
         }
-        public static string Annoy()
+        public static string Annoy(long chatID)
         {
             switch (RNG.NextNoMemory(0, 9))
             {
-                case 0: return "Чот лень";
-                case 1: return "Панда - щитпостер";
-                case 2: return "Не хочу";
-                case 3: return "Отстань";
-                case 4: return "Панда - главный щитпостер";
-                case 5: return "Ноборееее";
-                case 6: return "Ни";
-                case 7: return "Точно?";
-                case 8: return "А сам не можешь?";
+                case 0: return Localization.Get("annoy_1", chatID);
+                case 1: return Localization.Get("annoy_2", chatID);
+                case 2: return Localization.Get("annoy_3", chatID);
+                case 3: return Localization.Get("annoy_4", chatID);
+                case 4: return Localization.Get("annoy_5", chatID);
+                case 5: return Localization.Get("annoy_6", chatID);
+                case 6: return Localization.Get("annoy_7", chatID);
+                case 7: return Localization.Get("annoy_8", chatID);
+                case 8: return Localization.Get("annoy_9", chatID);
                 default: return string.Empty;
             }
         }
-        public static string RatingRepeat()
+        public static string RatingRepeat(long chatID)
         {
             switch (RNG.NextNoMemory(0, 9))
             {
-                case 0: return "Держи в курсе";
-                case 1: return "Я с первого раза понял";
-                case 2: return "Не хочу";
-                case 3: return "было)";
-                case 4: return "А тудути - евген";
-                case 5: return "Ты че шиз";
-                case 6: return "Ни";
-                case 7: return "заяц_адидас.вебп";
-                case 8: return "Ты уже голосовал";
+                case 0: return Localization.Get("rating_repeat_1", chatID);
+                case 1: return Localization.Get("rating_repeat_2", chatID);
+                case 2: return Localization.Get("rating_repeat_3", chatID);
+                case 3: return Localization.Get("rating_repeat_4", chatID);
+                case 4: return Localization.Get("rating_repeat_5", chatID);
+                case 5: return Localization.Get("rating_repeat_6", chatID);
+                case 6: return Localization.Get("rating_repeat_7", chatID);
+                case 7: return Localization.Get("rating_repeat_8", chatID);
+                case 8: return Localization.Get("rating_repeat_9", chatID);
                 default: return string.Empty;
             }
         }
--- den0bot/Locales/EN.cs
+++ den0bot/Locales/EN.cs
@@ -36,6 +36,48 @@ namespace den0bot.Locales
 			["girls_rating_delete_lowrating"] = "Girl has been deleted (rating lower -10)",
 			["girls_rating_delete_manual"] = "Girl has been deleted",
 			["girls_not_found"] = "No girls found",
+
+			/*
+			["event_1"] = "Егорыч, бросай наркотики",
+			["event_2"] = "Панда - щитпостер",
+			["event_3"] = "Не хочу",
+			["event_4"] = "Жаба, поебемся после работы?",
+			["event_5"] = "Панда - объебос",
+			["event_6"] = "Ноборееее",
+			["event_7"] = "Шлад, когда 900пп?",
+			["event_8"] = "ккк - самая лучшая девочка!",
+			["event_9"] = "Не получилось чет",
+
+			["annoy_1"] = "Чот лень",
+			["annoy_2"] = "Панда - щитпостер",
+			["annoy_3"] = "Не хочу",
+			["annoy_4"] = "Отстань",
+			["annoy_5"] = "Панда - главный щитпостер",
+			["annoy_6"] = "Ноборееее",
+			["annoy_7"] = "Ни",
+			["annoy_8"] = "Точно?",
+			["annoy_9"] = "А сам не можешь?",
+
+			["rating_repeat_1"] = "Держи в курсе",
+			["rating_repeat_2"] = "Я с первого раза понял",
+			["rating_repeat_3"] = "Не хочу",
+			["rating_repeat_4"] = "было)",
+			["rating_repeat_5"] = "А тудути - евген",
+			["rating_repeat_6"] = "Ты че шиз",
+			["rating_repeat_7"] = "Ни",
+			["rating_repeat_8"] = "заяц_адидас.вебп",
+			["rating_repeat_9"] = "Ты уже голосовал",
+
+			["basiccommands_help"] = "Дарова. Короче помимо того, что в списке команд я могу ещё:" + Environment.NewLine + Environment.NewLine +
+					"/addplayer <юзернейм> <имя> <osu!айди> - добавить игрока в базу. Бот будет следить за новыми топскорами и сообщать их в чат. Также имя используется в базе щитпостеров." + Environment.NewLine +
+					"/removeplayer <имя, указанное при добавлении> - убрать игрока из базы." + Environment.NewLine +
+					"/addmeme - добавить мемес базу, можно как ссылку на картинку из интернета, так и загрузить её самому, а команду прописать в подпись." + Environment.NewLine +
+					"/disableannouncements - отключить оповещения о новых скорах кукизи." + Environment.NewLine +
+					"/enableannouncements - включить их обратно." + Environment.NewLine +
+					"/setlocale <en/ru> - сменить язык" + Environment.NewLine +
+					"/delet в ответ на девку - удалить девку из базы." + Environment.NewLine + Environment.NewLine +
+					"Все эти команды доступны только админам конфы. По вопросам насчет бота писать @StanRiders, но лучше не писать.",
+			*/
 		};
 	}
 }
