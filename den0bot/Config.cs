b1a1387e66f3defb202fa728f8bd4c8f507f5d73
StanR <hi@stanr.info>
2018-11-29 14:02:31 +0300

    Const everything in config

--- den0bot/Config.cs
+++ den0bot/Config.cs
@@ -4,24 +4,24 @@ namespace den0bot
 	public static class Config
 	{
 		// https://telegram.me/botfather
-		public static readonly string telegam_token = string.Empty;
-		public static readonly string owner_username = "StanRiders";
+		public const string telegam_token = string.Empty;
+		public const string owner_username = "StanRiders";
 
 		// https://osu.ppy.sh/p/api
-		public static readonly string osu_token = string.Empty;
+		public const string osu_token = string.Empty;
 
 		// https://osu.ppy.sh/p/irc
-		public static readonly string osu_irc_username = string.Empty;
-		public static readonly string osu_irc_password = string.Empty;
+		public const string osu_irc_username = string.Empty;
+		public const string osu_irc_password = string.Empty;
 
-		public static readonly string osu_lobby_name = string.Empty;
-		public static readonly string osu_lobby_password = string.Empty;
+		public const string osu_lobby_name = string.Empty;
+		public const string osu_lobby_password = string.Empty;
 
 		// https://console.developers.google.com/apis/credentials
-		public static readonly string googleapi_token = string.Empty;
+		public const string googleapi_token = string.Empty;
 
 		// http://thecatapi.com/api-key-registration.html
-		public static readonly string cat_token = string.Empty;
+		public const string cat_token = string.Empty;
 
 	}
 }
