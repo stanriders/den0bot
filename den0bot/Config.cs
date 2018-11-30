f8f3c39592c745ef88c8ad406427c54a5cca5965
StanR <hi@stanr.info>
2018-11-30 13:57:30 +0300

    string.Empty isn't const

--- den0bot/Config.cs
+++ den0bot/Config.cs
@@ -4,24 +4,24 @@ namespace den0bot
 	public static class Config
 	{
 		// https://telegram.me/botfather
-		public const string telegam_token = string.Empty;
+		public const string telegam_token = "";
 		public const string owner_username = "StanRiders";
 
 		// https://osu.ppy.sh/p/api
-		public const string osu_token = string.Empty;
+		public const string osu_token = "";
 
 		// https://osu.ppy.sh/p/irc
-		public const string osu_irc_username = string.Empty;
-		public const string osu_irc_password = string.Empty;
+		public const string osu_irc_username = "";
+		public const string osu_irc_password = "";
 
-		public const string osu_lobby_name = string.Empty;
-		public const string osu_lobby_password = string.Empty;
+		public const string osu_lobby_name = "";
+		public const string osu_lobby_password = "";
 
 		// https://console.developers.google.com/apis/credentials
-		public const string googleapi_token = string.Empty;
+		public const string googleapi_token = "";
 
 		// http://thecatapi.com/api-key-registration.html
-		public const string cat_token = string.Empty;
+		public const string cat_token = "";
 
 	}
 }
