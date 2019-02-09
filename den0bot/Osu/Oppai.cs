898a34e1b5a18e31455b8ba228b472ebb99eff05
StanR <hi@stanr.info>
2019-02-09 16:51:43 +0300

    Switch to new OppaiSharp

--- den0bot/Osu/Oppai.cs
+++ den0bot/Osu/Oppai.cs
@@ -17,12 +17,12 @@ namespace den0bot.Osu
 		public static OppaiInfo GetBeatmapOppaiInfo(Map map, Score score = null)
 		{
 			if (score != null)
-				return GetBeatmapOppaiInfo(map.FileBytes, score.EnabledMods ?? Mods.None, -1, (int)score.Count300, (int)score.Count100, (int)score.Count50, (int)score.Combo, (int)score.Misses);
+				return GetBeatmapOppaiInfo(map.FileBytes, score.EnabledMods ?? Mods.None, score.Accuracy, (int)score.Combo, (int)score.Misses);
 			else
 				return GetBeatmapOppaiInfo(map.FileBytes);
 		}
 
-		public static OppaiInfo GetBeatmapOppaiInfo(byte[] beatmap, Mods mods = Mods.None, double acc = -1, int c300 = -1, int c100 = 0, int c50 = 0, int combo = -1, int misses = 0)
+		public static OppaiInfo GetBeatmapOppaiInfo(byte[] beatmap, Mods mods = Mods.None, double acc = -1, int combo = -1, int misses = 0)
 		{
 			try
 			{
@@ -33,16 +33,8 @@ namespace den0bot.Osu
 				Beatmap map = Beatmap.Read(reader);
 				DiffCalc diff = new DiffCalc().Calc(map, (OppaiSharp.Mods)mods);
 
-				if (acc != -1)
-				{
-					int objectCount = map.CountCircles + map.CountSliders + map.CountSpinners;
-					AccuracyToHits(acc, objectCount, misses, out c300, out c100, out c50);
-				}
-
 				PPv2 pp = new PPv2(new PPv2Parameters(map, diff,
-					c300: c300,
-					c100: c100,
-					c50: c50,
+					accuracy: acc / 100,
 					cMiss: misses,
 					combo: combo,
 					mods: (OppaiSharp.Mods)mods)
@@ -60,41 +52,6 @@ namespace den0bot.Osu
 			catch (Exception) { }
 
 			return null;
-		}
-
-		private static void AccuracyToHits(double acc, int objCount, int misses, out int count300, out int count100, out int count50)
-		{
-			// straight outta oppai-ng
-			double maxacc = 100; // TODO: actual maxacc!!!!!!!!!
-			misses = Math.Min(objCount, misses);
-			int max300 = objCount - misses;
-			double accuracy = Math.Max(0.0, Math.Min(maxacc, acc));
-
-			int c50 = 0;
-
-			/* just some black magic maths from wolfram alpha */
-			int c100 = (int)
-				Math.Floor(-3.0 * ((accuracy * 0.01 - 1.0) *
-					objCount + misses) * 0.5);
-
-			if (c100 > objCount - misses)
-			{
-				/* acc lower than all 100s, use 50s */
-				c100 = 0;
-				c50 = (int)
-					Math.Floor(-6.0 * ((accuracy * 0.01 - 1.0) *
-						objCount + misses) * 0.2);
-
-				c50 = Math.Min(max300, c50);
-			}
-			else
-			{
-				c100 = Math.Min(max300, c100);
-			}
-
-			count300 = objCount - c100 - c50 - misses;
-			count100 = c100;
-			count50 = c50;
 		}
 	}
 }
