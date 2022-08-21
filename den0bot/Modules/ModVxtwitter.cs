9d8aa712968a0b446b57e9882a0e39f1b3416fb1
StanR <hi@stanr.info>
2022-08-21 16:58:17 +0300

    Vxtwitter module

---
+++ den0bot/Modules/ModVxtwitter.cs
@@ -0,0 +1,31 @@
+// den0bot (c) StanR 2022 - MIT License
+using System;
+using System.Linq;
+using Telegram.Bot.Types;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using den0bot.Util;
+using den0bot.Types;
+
+namespace den0bot.Modules
+{
+	internal class ModVxtwitter : IModule, IReceiveAllMessages
+	{
+        private readonly Regex regex = new(@".+\/\/twitter\.com\/(.+)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+		public async Task ReceiveMessage(Message message)
+		{
+			if (string.IsNullOrEmpty(message.Text))
+				return;
+
+            Match regexMatch = regex.Match(message.Text);
+			if (regexMatch.Groups.Count > 1)
+			{
+                var tail = regexMatch.Groups.Values[1];
+
+                await API.SendMessage($"https://vxtwitter.com/{tail}", message.Chat.Id, replyToId: message.MessageId);
+            }
+		}
+	}
+}
