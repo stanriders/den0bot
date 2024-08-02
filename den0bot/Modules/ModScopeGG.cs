5c2de52ce5a07135a341c933b234d711cb37d3e5
StanR <hi@stanr.info>
2024-08-02 22:26:02 +0500

    Fix scopegg module throwing exception of wrong link

--- den0bot/Modules/ModScopeGG.cs
+++ den0bot/Modules/ModScopeGG.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿// den0bot (c) StanR 2024 - MIT License
+using System;
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -8,8 +9,15 @@ using Telegram.Bot.Types;
 
 namespace den0bot.Modules
 {
-	internal partial class ModScopeGG(ILogger<IModule> logger) : IModule(logger), IReceiveAllMessages
+	internal partial class ModScopeGG : IModule, IReceiveAllMessages
 	{
+		private readonly ILogger<IModule> logger;
+
+		public ModScopeGG(ILogger<IModule> logger) : base(logger)
+		{
+			this.logger = logger;
+		}
+
 		[GeneratedRegex(@"(https:\/\/app\.scope\.gg([\w/]*[\w/])?)")]
 		private static partial Regex ScopeLinkRegex();
 
@@ -36,15 +44,22 @@ namespace den0bot.Modules
 				var match = ClipLinkRegex().Match(page);
 				var videoLink = string.Concat(ClipLinkReplaceRegex().Replace(match.Value, "clips"), ".mp4");
 
-				var clipMatch = ClipTitleRegex().Match(page);
-				var caption = clipMatch.Success ? clipMatch.Groups[1].Value : "VAC";
-
-				if (!await API.SendVideo(videoLink, message.Chat.Id, caption, message.MessageId))
+				if (Uri.TryCreate(videoLink, UriKind.RelativeOrAbsolute, out var videoUri))
 				{
-					// telegram couldn't download the video by itself, stream it manually
-					var video = await client.GetStreamAsync(videoLink);
+					var clipMatch = ClipTitleRegex().Match(page);
+					var caption = clipMatch.Success ? clipMatch.Groups[1].Value : "VAC";
 
-					await API.SendVideo(video, message.Chat.Id, caption, message.MessageId);
+					if (!await API.SendVideo(videoLink, message.Chat.Id, caption, message.MessageId))
+					{
+						// telegram couldn't download the video by itself, stream it manually
+						var video = await client.GetStreamAsync(videoUri);
+
+						await API.SendVideo(video, message.Chat.Id, caption, message.MessageId);
+					}
+				}
+				else
+				{
+					logger.LogError("Invalid scope.gg link {Link}", videoLink);
 				}
 			}
 		}
