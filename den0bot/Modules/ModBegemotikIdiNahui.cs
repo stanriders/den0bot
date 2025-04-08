16d0cdc1144214e9a1a03373074c9becb4d0e7c0
StanR <hi@stanr.info>
2025-04-08 19:12:26 +0500

    Update packages

--- den0bot/Modules/ModBegemotikIdiNahui.cs
+++ den0bot/Modules/ModBegemotikIdiNahui.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using den0bot.Types;
 using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
@@ -8,15 +9,37 @@ namespace den0bot.Modules
 {
 	public class ModBegemotikIdiNahui(ILogger<IModule> logger) : IModule(logger), IReceiveAllMessages
 	{
+		private static string[] knownPacks = new[]
+		{
+			"Hippo4_0734_Nyasticks",
+			"Baby_chubby_Hippo",
+			"hippo_cute_bunny_Nyasticks",
+			"Paired_Hippo_Nyasticks2",
+			"baby_hippo_Nyasticks",
+			"hippo3_Nyasticks",
+			"Begemotik_2"
+		};
+
 		public async Task ReceiveMessage(Message message)
 		{
-			if (message.Sticker?.SetName != null && 
-				message.Sticker.SetName.Contains("бегемо", StringComparison.InvariantCultureIgnoreCase) && 
-				message.Sticker.SetName.Contains("nyasticks", StringComparison.InvariantCultureIgnoreCase))
+			if (message.Sticker?.SetName != null)
 			{
-				await API.SendSticker(
-					new InputFileId("CAACAgIAAxkBAAEOQsRn9Si9wV_SU5o1IAVLjOPQxiFPkgACFGYAAnewwUqltednG_eKXzYE"),
-					message.Chat.Id, message.MessageId);
+				if (knownPacks.Contains(message.Sticker.SetName))
+				{
+					await API.SendSticker(
+						new InputFileId("CAACAgIAAxkBAAEOQsRn9Si9wV_SU5o1IAVLjOPQxiFPkgACFGYAAnewwUqltednG_eKXzYE"),
+						message.Chat.Id, message.MessageId);
+
+					return;
+				}
+
+				if (message.Sticker.SetName.Contains("hippo", StringComparison.InvariantCultureIgnoreCase) &&
+				    message.Sticker.SetName.Contains("nyasticks", StringComparison.InvariantCultureIgnoreCase))
+				{
+					await API.SendSticker(
+						new InputFileId("CAACAgIAAxkBAAEOQsRn9Si9wV_SU5o1IAVLjOPQxiFPkgACFGYAAnewwUqltednG_eKXzYE"),
+						message.Chat.Id, message.MessageId);
+				}
 			}
 		}
 	}
