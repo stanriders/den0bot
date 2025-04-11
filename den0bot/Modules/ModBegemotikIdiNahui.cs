5a43b74ef6c4dee3a6b51b4b4ebfd7a44f2202d7
StanR <hi@stanr.info>
2025-04-11 17:32:51 +0500

    begemotik_idi_nahuy_idi_nahuy_idi_nahuy

--- den0bot/Modules/ModBegemotikIdiNahui.cs
+++ den0bot/Modules/ModBegemotikIdiNahui.cs
@@ -7,7 +7,7 @@ using Telegram.Bot.Types;
 
 namespace den0bot.Modules
 {
-	public class ModBegemotikIdiNahui(ILogger<IModule> logger) : IModule(logger), IReceiveAllMessages
+	public class ModBegemotikIdiNahui(ILogger<IModule> logger) : IModule(logger), IReceiveAllMessages, IReceiveForwards
 	{
 		private static string[] knownPacks = new[]
 		{
@@ -20,26 +20,61 @@ namespace den0bot.Modules
 			"Begemotik_2"
 		};
 
+		private const string begemotik_idi_nahuy =
+			"CAACAgIAAx0EUctp0AACIypn-QsKtcEC7dtCbYl26fn_48jvFgACFGYAAnewwUqltednG_eKXzYE";
+
+		private const string begemotik_idi_nahuy_idi_nahuy =
+			"CAACAgIAAx0CUctp0AACIy9n-QtAdBOUPD15KAFdeD1gYqOXagAC-3QAAnFkwUrqtAGrfploezYE";
+
+		private const string begemotik_idi_nahuy_idi_nahuy_idi_nahuy =
+			"CAACAgIAAxkBAAEOSBNn-QiTND5o09gWAVsR6woV0NLdCgACxV8AAkaUwEoNGnCBIHKe6DYE";
+
+		private const long ceo_of_life = -1001269393237;
+
 		public async Task ReceiveMessage(Message message)
 		{
-			if (message.Sticker?.SetName != null)
+			if (message.Sticker?.FileId == begemotik_idi_nahuy_idi_nahuy &&
+			    message.ReplyToMessage?.Sticker?.FileId == begemotik_idi_nahuy &&
+			    message.ReplyToMessage?.From?.Id == API.BotUser.Id)
 			{
-				if (knownPacks.Contains(message.Sticker.SetName))
+				await API.SendSticker(
+					new InputFileId(begemotik_idi_nahuy_idi_nahuy_idi_nahuy),
+					message.Chat.Id, message.MessageId);
+
+				return;
+			}
+
+			var sticker = message.Sticker ?? message.ReplyToMessage?.Sticker;
+
+			if (sticker?.SetName != null)
+			{
+				if (knownPacks.Contains(sticker.SetName))
 				{
 					await API.SendSticker(
-						new InputFileId("CAACAgIAAxkBAAEOQsRn9Si9wV_SU5o1IAVLjOPQxiFPkgACFGYAAnewwUqltednG_eKXzYE"),
+						new InputFileId(begemotik_idi_nahuy),
 						message.Chat.Id, message.MessageId);
 
 					return;
 				}
 
-				if (message.Sticker.SetName.Contains("hippo", StringComparison.InvariantCultureIgnoreCase) &&
-				    message.Sticker.SetName.Contains("nyasticks", StringComparison.InvariantCultureIgnoreCase))
+				if (sticker.SetName.Contains("hippo", StringComparison.InvariantCultureIgnoreCase) &&
+				    sticker.SetName.Contains("nyasticks", StringComparison.InvariantCultureIgnoreCase))
 				{
 					await API.SendSticker(
-						new InputFileId("CAACAgIAAxkBAAEOQsRn9Si9wV_SU5o1IAVLjOPQxiFPkgACFGYAAnewwUqltednG_eKXzYE"),
+						new InputFileId(begemotik_idi_nahuy),
 						message.Chat.Id, message.MessageId);
+
+					return;
 				}
+			}
+
+			if (message.ForwardFromChat?.Id == ceo_of_life)
+			{
+				await API.SendSticker(
+					new InputFileId("CAACAgIAAxkBAAEOSCdn-QspIzkRlLL81Ucr6xS5093tRwACs2UAAvXjwEqlrXpLqGMgHTYE"),
+					message.Chat.Id, message.MessageId);
+
+				return;
 			}
 		}
 	}
